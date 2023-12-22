230b008a8f91ecaffabcaecc524d942b2ab97ff6
Walber <walber.araujo@getcandidly.com>
2023-12-22 13:59:54 -0300

    updated timer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import { Play } from "phosphor-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
+import { differenceInSeconds } from "date-fns";
 import * as zod from "zod";
 
 import {
@@ -21,14 +23,20 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, "The cycle must be a maximum of 60 minutes"),
 });
 
-// interface newCycleFormData {
-//   task: string;
-//   minutesAmount: number;
-// }
-
 type newCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 
+interface CycleFormat {
+  id: string;
+  task: string;
+  minutesAmount: number;
+  startDate: Date;
+}
+
 export function Home() {
+  const [cycles, setCycles] = useState<CycleFormat[]>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+  const [secondsPassed, setSecondsPassed] = useState(0);
+
   const { register, handleSubmit, watch, reset } = useForm<newCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -37,10 +45,55 @@ export function Home() {
     },
   });
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  useEffect(() => {
+    let interval: number;
+
+    if (activeCycle) {
+      interval = setInterval(() => {
+        setSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate),
+        );
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [activeCycle]);
+
   function handleCreateNewTimer(data: newCycleFormData) {
-    console.log(data);
+    const id = String(new Date().getTime());
+
+    const newCycle: CycleFormat = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    };
+
+    setCycles((state) => [...state, newCycle]);
+    setActiveCycleId(id);
+    setSecondsPassed(0);
+
     reset();
   }
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  const currentSeconds = activeCycle ? totalSeconds - secondsPassed : 0;
+
+  const minutesAmountToDisplay = Math.floor(currentSeconds / 60);
+  const secondsAmountToDisplay = currentSeconds % 60;
+
+  const minutes = String(minutesAmountToDisplay).padStart(2, "0");
+  const seconds = String(secondsAmountToDisplay).padStart(2, "0");
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`;
+    }
+  }, [minutes, seconds, activeCycle]);
 
   const task = watch("task");
   const haveTaskToDisable = !task;
@@ -81,11 +134,11 @@ export function Home() {
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
 
         <StartCountdownButton
